6b2fd94001e88b2784b317985e50c8d55be8cce1
Sadia Bennthe Azad <tanzibamouri00@gmail.com>
2023-12-30 17:10:44 +0600

    feat:willpop

--- lib/pages/page_with_navigation.dart
+++ lib/pages/page_with_navigation.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mh_core/widgets/button/custom_button.dart';
 import 'package:perfecto/constants/color_constants.dart';
 import 'package:perfecto/controller/navigation_controller.dart';
 import 'package:perfecto/drawer/custom_drawer.dart';
@@ -21,54 +24,121 @@ class MainHomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.kBackgroundColor,
       drawer: CustomDrawer(),
-      body: Obx(
-        () {
-          switch (controller.selectedIndex.value) {
-            case 0:
-              return const HomeScreen();
-            case 1:
-              return const CategoryScreen();
-            case 2:
-              return const ChatScreen();
-            case 3:
-              return const ProfileScreen();
-            default:
-              return const HomeScreen();
-          }
+      body: WillPopScope(
+        onWillPop: () async{
+          openDialog(context);
+
+          return Future.value(false);
         },
+        child: Obx(
+          () {
+            switch (controller.selectedIndex.value) {
+              case 0:
+                return const HomeScreen();
+              case 1:
+                return const CategoryScreen();
+              case 2:
+                return const ChatScreen();
+              case 3:
+                return const ProfileScreen();
+              default:
+                return const HomeScreen();
+            }
+          },
+        ),
       ),
       bottomNavigationBar: Obx(() {
-    return Container(
-    height: 70,
-    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-    decoration: BoxDecoration(
-    boxShadow: [BoxShadow(blurRadius: 12, color: Colors.black.withOpacity(.08))],
-    color: Colors.white,
-    borderRadius: const BorderRadius.vertical(top: Radius.circular(15))),
-    child: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: [
-    ...List.generate(controller.navList.length, (index) {
-    final data = controller.navList[index];
-    return GestureDetector(
-    onTap: () {
-    // index=controller.selectedIndex.value;
-    print('Tab tapped: ${index}');
+        return Container(
+          height: 70,
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(blurRadius: 12, color: Colors.black.withOpacity(.08))
+              ],
+              color: Colors.white,
+              borderRadius:
+                  const BorderRadius.vertical(top: Radius.circular(15))),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ...List.generate(controller.navList.length, (index) {
+                final data = controller.navList[index];
+                return GestureDetector(
+                    onTap: () {
+                      // index=controller.selectedIndex.value;
+                      print('Tab tapped: ${index}');
 
-    controller.changeTabIndex(index);
-    },
-    child: NavWidget(
-    title: data['title'] + (index == 2 ? '(0)' : ''),
-    icon: data['icon'],
-    isSelected: controller.selectedIndex.value == index,
-    ));
-    })
-    ],
-    ),
-    );
-    }),
+                      controller.changeTabIndex(index);
+                    },
+                    child: NavWidget(
+                      title: data['title'] + (index == 2 ? '(0)' : ''),
+                      icon: data['icon'],
+                      isSelected: controller.selectedIndex.value == index,
+                    ));
+              })
+            ],
+          ),
+        );
+      }),
     );
   }
+
+  Future<void> openDialog(BuildContext context) async{ switch (await showDialog(
+  context: context,
+  builder: (context) {
+  return SimpleDialog(surfaceTintColor: Colors.white,
+  clipBehavior: Clip.hardEdge,
+  titlePadding: EdgeInsets.zero,
+  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+  children: [
+  Padding(
+  padding: const EdgeInsets.all(16.0),
+  child: Container(
+  alignment: Alignment.center,
+  width: MediaQuery.of(context).size.width / 2,
+  child: Text(
+  'Are you sure to exit app?',
+  style: AppTheme.textStyleMediumBlack12,
+  ),
+  ),
+  ),
+  Row(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [
+  CustomButton(
+  onPressed: () {
+  Navigator.pop(context, 0);
+  },
+  width: MediaQuery.of(context).size.width / 6,
+  labelColor: AppColors.kPrimaryColor,
+  boxShadowColor: Colors.transparent,
+  isBorder: true,
+  borderColor: AppColors.kPrimaryColor,
+  primary: Colors.white,
+  marginVertical: 6,
+  marginHorizontal: 0,
+  borderRadiusAll: 22,
+  label: 'Cancel'),
+  CustomButton(
+  onPressed: () {
+  Navigator.pop(context, 1);
+  },
+  width: MediaQuery.of(context).size.width / 6,
+  marginVertical: 6,
+  marginHorizontal: 6,
+  borderRadiusAll: 22,
+  label: 'Exit')
+  ],
+  )
+  ],
+  );
+  },
+    )) {
+    case 0:
+    break;
+    case 1:
+    exit(0);
+    }}
 }
 
 class NavWidget extends StatelessWidget {
@@ -88,7 +158,9 @@ class NavWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       child: Container(
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(4), color: isSelected ? const Color(0xffF0F4F5) : Colors.transparent),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4),
+            color: isSelected ? const Color(0xffF0F4F5) : Colors.transparent),
         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
         child: Row(
           children: [
@@ -108,7 +180,10 @@ class NavWidget extends StatelessWidget {
                 isChat
                     ? const Text(
                         '(0)',
-                        style: TextStyle(color: AppColors.kPrimaryColor, fontSize: 10, fontWeight: FontWeight.w600),
+                        style: TextStyle(
+                            color: AppColors.kPrimaryColor,
+                            fontSize: 10,
+                            fontWeight: FontWeight.w600),
                       )
                     : const SizedBox.shrink(),
               ],
@@ -140,11 +215,16 @@ class PageWithNavigation extends StatelessWidget {
             return Material(
               child: Container(
                 height: 70,
-                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
                 decoration: BoxDecoration(
-                    boxShadow: [BoxShadow(blurRadius: 12, color: Colors.black.withOpacity(.08))],
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 12, color: Colors.black.withOpacity(.08))
+                    ],
                     color: Colors.white,
-                    borderRadius: const BorderRadius.vertical(top: Radius.circular(15))),
+                    borderRadius:
+                        const BorderRadius.vertical(top: Radius.circular(15))),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
